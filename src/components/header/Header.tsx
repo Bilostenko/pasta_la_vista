6f8cf0b5c6561b0ed3bf4ea90733890d8a83214a
Bilostenko <bilostenkod@gmail.com>
2024-08-16 16:45:02 +0300

    header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,33 @@
 import './header.css'
+import logo from '../../assets/logo.jpg'
+import phone from '../../assets/phone-icon.png'
+import cart from '../../assets/cart-icon.png'
+
+
 const Header = () => {
 
   return (
     <header className="header">
       <div className="container">
+        <div className="header__row">
+          <div className="header__logo">
+            <img src={logo} alt="logo" />
+            <span>PASTA</span>
+            <p>la vista baby</p>
+          </div>
+          <nav className="header__nav">
+            <ul>
+              <li ><a href="#" className='nunito'>About us</a></li>
+              <li ><a href="#" className='nunito'>Menu</a></li>
+              <li><a href="#" className='nunito'>Contacts</a></li>
+              <li><a href="#" className='nunito'>Table reserve</a></li>
+            </ul>
+          </nav>
+          <div className="header__actionBtns">
+            <img src={phone} alt="phone" />
+            <img src={cart} alt="cart" />
+          </div>
+        </div>
       </div>
     </header>
   )
